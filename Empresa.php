48d44655e7201dd1e6df7eea433d0b46f0249f25
Pedro Lucas <izielucas2016@gmail.com>
2025-07-07 14:56:49 -0300

    feat: created Empresa class

---
+++ Empresa.php
@@ -0,0 +1,16 @@
+<?php
+
+class Empresa{
+    private string $id;
+    private string $logo;
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+}
